4d5eb1c49587bc394d4ff8e7503f05ab860dcfa0
Ellen7ions <lzz765177255@gmail.com>
2021-04-10 19:50:51 +0800

    modify nothing

--- src/main/java/edu/ujn/ExpertMachine/controller/GetGraphServlet.java
+++ src/main/java/edu/ujn/ExpertMachine/controller/GetGraphServlet.java
@@ -13,10 +13,11 @@ public class GetGraphServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doPost(request, response);
+        //
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         request.setCharacterEncoding("UTF-8");
         response.setContentType("text/html;charset=utf-8");
         response.setCharacterEncoding("UTF-8");
